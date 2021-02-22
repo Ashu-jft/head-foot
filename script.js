e3d754770da9efc4bae8c6490e4d8c103fd027a8
Ashu Babbar <ashu.babbar@jellyfishtechnologies.com>
2021-02-22 18:00:48 +0530

    added header footer and table

---
+++ script.js
@@ -0,0 +1,8 @@
+let url = 'https://jsonplaceholder.typicode.com/users';
+
+fetch(url)
+.then(res => res.json())
+.then((out) => {
+  console.log('Checkout this JSON! ', out);
+})
+.catch(err => { throw err });
